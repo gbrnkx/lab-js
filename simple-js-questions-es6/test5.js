b6716aa4be154d2c48b8ae46bffbe3ee1bf18c40
Gabriel Pacheco <pacheco.sga@gmail.com>
2019-09-27 01:25:10 -0500

    Lab-js test5 DONE

--- simple-js-questions-es6/test5.js
+++ simple-js-questions-es6/test5.js
@@ -6,18 +6,32 @@ const getSumAsync = async (num1, num2) => {
   });
 };
 
+const getProdAsync = async (num1, num2) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(num1 * num2);
+    }, 2000);
+  });
+};
 
-const start = () => {
 
-  const sum = getSumAsync(3, 5);
-  console.log('The sum was: ', sum);
+const start = async() => {
 
-  // We expect that sum will have the value 8, after 3 seconds, but what we see printed in the console is:
-  // The sum was:  Promise {<pending>}
-  // 1. What did we do wrong? How can it be fixed?
-  // 2. Create another async function, getProdAsync, which returns the multiplication result of the 2 numbers
-  //    and returns the result in 2 seconds
-  // 3. Call both async functions and return the result of whichever function returns first  (optional)
+  const sum = await getSumAsync(3,5);
+
+  const prod = await getProdAsync(3,5);
+
+  console.log('The result was: ', sum | prod);
+
+/*
+We expect that sum will have the value 8, after 3 seconds, but what we see printed in the console is:
+The sum was:  Promise {<pending>}
+1. What did we do wrong? How can it be fixed?
+   The error is two missing key words, "async" in the start method and "await" when we call the async function.
+2. Create another async function, getProdAsync, which returns the multiplication result of the 2 numbers
+   and returns the result in 2 seconds
+3. Call both async functions and return the result of whichever function returns first  (optional)
+*/
 }
 
 
